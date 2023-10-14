fe89fa98f7a14d264fa5d27a2d1b54760fc0bca2
Amanda Zanata <amandazanata46@gmail.com>
2023-10-14 18:04:42 -0300

    requisito 9

--- news_rest/serializers/users_serializer.py
+++ news_rest/serializers/users_serializer.py
@@ -1,4 +1,4 @@
-""" from rest_framework import serializers
+from rest_framework import serializers
 from news.models import Users
 
 
@@ -6,4 +6,3 @@ class UsersSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Users
         fields = ["id", "name", "email", "role"]
-"""
